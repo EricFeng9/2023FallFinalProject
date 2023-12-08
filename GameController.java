cc04bf4fe3da25301e7450b74a9983d014bf6d26
Eric_Feng <2504170651@qq.com>
2023-12-08 17:42:59 +0800

    Add files via upload

---
+++ GameController.java
@@ -0,0 +1,139 @@
+package controller;
+
+import listener.GameListener;
+import model.Constant;
+import model.Chessboard;
+import model.ChessboardPoint;
+import view.CellComponent;
+import view.ChessComponent;
+import view.ChessboardComponent;
+
+/**
+ * Controller is the connection between model and view,
+ * when a Controller receive a request from a view, the Controller
+ * analyzes and then hands over to the model for processing
+ * [in this demo the request methods are onPlayerClickCell() and
+ * onPlayerClickChessPiece()]
+ *
+ */
+public class GameController implements GameListener {
+
+    private Chessboard model;
+    private ChessboardComponent view;
+
+    // Record whether there is a selected piece before
+    private ChessboardPoint selectedPoint;
+    private ChessboardPoint selectedPoint2;
+
+    public GameController(ChessboardComponent view, Chessboard model) {
+        this.view = view;
+        this.model = model;
+
+        view.registerController(this);
+        initialize();
+        view.initiateChessComponent(model);
+        view.repaint();
+    }
+
+    private void initialize() {
+        for (int i = 0; i < Constant.CHESSBOARD_ROW_SIZE.getNum(); i++) {
+            for (int j = 0; j < Constant.CHESSBOARD_COL_SIZE.getNum(); j++) {
+
+            }
+        }
+    }
+
+    // click an empty cell
+    @Override
+    public void onPlayerClickCell(ChessboardPoint point, CellComponent component) {
+
+    }
+
+    @Override
+    public void onPlayerSwapChess(){
+        // TODO: Init your swap function here.
+        System.out.println("Implement your swap here.");
+        //model层交换（是否能交换要自己判断）
+        if (selectedPoint!= null && selectedPoint2!= null){
+            model.swapChessPiece(selectedPoint,selectedPoint2);
+        }
+
+    }
+
+    @Override
+    public void onPlayerNextStep(){
+        // TODO: Init your next step function here.
+        System.out.println("Implement your next step here.");
+    }
+
+    // click a cell with a chess
+    @Override
+    public void onPlayerClickChessPiece(ChessboardPoint point, ChessComponent component) {
+        if(selectedPoint2 != null){
+            var distance2point1 = Math.abs(selectedPoint.getCol() - point.getCol()) + Math.abs(selectedPoint.getRow() - point.getRow());
+            var distance2point2 = Math.abs(selectedPoint2.getCol() - point.getCol()) + Math.abs(selectedPoint2.getRow() - point.getRow());
+            var point1 = (ChessComponent)view.getGridComponentAt(selectedPoint).getComponent(0);
+            var point2 = (ChessComponent)view.getGridComponentAt(selectedPoint2).getComponent(0);
+            if(distance2point1 == 0 && point1!= null){
+                point1.setSelected(false);
+                point1.repaint();
+                selectedPoint = selectedPoint2;
+                selectedPoint2 = null;
+            }else if(distance2point2 == 0 && point2!= null){
+                point2.setSelected(false);
+                point2.repaint();
+                selectedPoint2 = null;
+            }else if(distance2point1 == 1 && point2!= null){
+                point2.setSelected(false);
+                point2.repaint();
+                selectedPoint2 = point;
+                component.setSelected(true);
+                component.repaint();
+            }else if(distance2point2 == 1 && point1!= null){
+                point1.setSelected(false);
+                point1.repaint();
+                selectedPoint = selectedPoint2;
+                selectedPoint2 = point;
+                component.setSelected(true);
+                component.repaint();
+            }
+            return;
+        }
+
+        
+        if (selectedPoint == null) {
+            selectedPoint = point;
+            component.setSelected(true);
+            component.repaint();
+            return;
+        }
+        
+        var distance2point1 = Math.abs(selectedPoint.getCol() - point.getCol()) + Math.abs(selectedPoint.getRow() - point.getRow()); 
+        
+        if(distance2point1 == 0){
+            selectedPoint = null;
+            component.setSelected(false);
+            component.repaint();
+            return;
+        }
+
+        if(distance2point1 == 1){
+            selectedPoint2 = point;
+            component.setSelected(true);
+            component.repaint();
+        }else{
+            selectedPoint2 = null;
+            
+            var grid = (ChessComponent)view.getGridComponentAt(selectedPoint).getComponent(0);
+            if(grid == null) return;            
+            grid.setSelected(false);
+            grid.repaint();
+            
+            selectedPoint = point;
+            component.setSelected(true);
+            component.repaint();
+        }
+            
+        
+    }
+}
