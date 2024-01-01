1ba18fc21b4fc4bf82812efa76ba507a0b4b0c9d
Eric_Feng <2504170651@qq.com>
2024-01-01 17:41:23 +0800

    Delete src directory

--- src/model/Util.java
+++
@@ -1,8 +0,0 @@
-package model;
-//finish
-public class Util {
-    public static <T> T RandomPick(T[] arr){
-        int randomIndex = (int) (Math.random() * arr.length);
-        return arr[randomIndex];
-    }
-}
